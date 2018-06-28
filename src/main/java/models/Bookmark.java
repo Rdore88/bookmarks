c35824e077cc80244ebccf0f413045726e74a011
Robby Dore <rdore88@gmail.com>
2018-06-28 17:01:44 -0400

    Basic app working

---
+++ src/main/java/models/Bookmark.java
@@ -0,0 +1,48 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Bookmark {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Account account;
+
+    private String uri;
+
+    private String descripion;
+
+    private Bookmark() {}
+
+    public Bookmark(final Account account, final String uri, final String description){
+        this.account = account;
+        this.uri = uri;
+        this.descripion = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getDescripion() {
+        return descripion;
+    }
+}
